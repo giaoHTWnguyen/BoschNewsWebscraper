9020bfae2032a558425867004b2b373e681d0c19
Giao Nguyen <s0573654@htw-berlin.de>
2023-07-27 23:14:47 +0200

    authors

--- modules/template_scraper.py
+++
@@ -1,7 +0,0 @@
-import database.articleData
-
-def template_scraper(url, options):
-    # run scrapping
-    founddata = new articleData
-    result_of_found_articleData = list...
-    return result_of_found_articleData
